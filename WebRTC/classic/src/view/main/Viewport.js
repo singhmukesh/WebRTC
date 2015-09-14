c1b6ae4d106fbf8340c0e798ab49ec3632714843
Steffen Hiller <s.hiller@360releases.com>
2015-09-14 18:31:00 +0200

    Added flexible toolbar and styled primary toolbar.

--- WebRTC/classic/src/view/main/Viewport.js
+++ WebRTC/classic/src/view/main/Viewport.js
@@ -11,9 +11,9 @@ Ext.define('WebRTC.view.main.Viewport', {
         //'Material.container.plugin.FlexibleToolbar'
     ],
 
-    //plugins: [
-    //    'materialflexibletoolbar'
-    //],
+    plugins: [
+        'materialflexibletoolbar'
+    ],
 
     controller: 'mainviewport',
 
@@ -23,71 +23,75 @@ Ext.define('WebRTC.view.main.Viewport', {
 
     tbar: {
         height: 64,
-        items: [
-        {
-            iconCls: 'md-add',
-            plain: true,
-            listeners: {
-                click: 'onRoomAdd'
-            }
-        },//{
-        //    iconCls: 'x-fa fa-pencil',
-        //    plain: true,
-        //    bind:{
-        //        disabled: '{!isRoomSelected}'
-        //    },
-        //    listeners: {
-        //        click: 'onRoomEdit'
-        //    }
-        //},{
-        //    xtype: 'combobox',
-        //    reference: 'roomscombo',
-        //    bind:{
-        //        store: '{rooms}'
-        //    },
-        //    queryMode: 'local',
-        //    displayField: 'name',
-        //    valueNotFoundText: '',
-        //    emptyText: 'select a room...',
-        //    valueField: 'id',
-        //    listeners: {
-        //        select: 'onRoomSelect'
-        //    }
-        //},{
-        //    iconCls: 'x-fa fa-trash-o',
-        //    plain: true,
-        //    bind:{
-        //        disabled: '{!isRoomSelected}'
-        //    },
-        //    listeners: {
-        //        click: 'onRoomRemove'
-        //    }
-        //}
-        '->',
-        {
-            iconCls: 'md-settings',
-            //bind:{
-            //    text: '{name}'
-            //},
-            handler: 'onSettingsUserSelect'
+        ui: 'primary',
+        defaults: {
+            ui: 'primary-toolbar'
         },
-        {
-            iconCls: 'md-fullscreen',
-            handler: 'onToggleFullScreen'
-        },{
-            iconCls: 'x-fa fa-gear',
-            bind:{
-                hidden: '{isAdmin}'
+        items: [
+            {
+                iconCls: 'md-add',
+                plain: true,
+                listeners: {
+                    click: 'onRoomAdd'
+                }
+            },//{
+            //    iconCls: 'x-fa fa-pencil',
+            //    plain: true,
+            //    bind:{
+            //        disabled: '{!isRoomSelected}'
+            //    },
+            //    listeners: {
+            //        click: 'onRoomEdit'
+            //    }
+            //},{
+            //    xtype: 'combobox',
+            //    reference: 'roomscombo',
+            //    bind:{
+            //        store: '{rooms}'
+            //    },
+            //    queryMode: 'local',
+            //    displayField: 'name',
+            //    valueNotFoundText: '',
+            //    emptyText: 'select a room...',
+            //    valueField: 'id',
+            //    listeners: {
+            //        select: 'onRoomSelect'
+            //    }
+            //},{
+            //    iconCls: 'x-fa fa-trash-o',
+            //    plain: true,
+            //    bind:{
+            //        disabled: '{!isRoomSelected}'
+            //    },
+            //    listeners: {
+            //        click: 'onRoomRemove'
+            //    }
+            //}
+            '->',
+            {
+                iconCls: 'md-settings',
+                //bind:{
+                //    text: '{name}'
+                //},
+                handler: 'onSettingsUserSelect'
             },
-            handler: 'onSettingsAdminSelect'
-        }
-        //,{
-        //    style:'background-image: url(/static/images/TokBoxIcon.png) !important; background-size: 29px 29px; background-repeat: no-repeat; ',
-        //    plain: true,
-        //    listeners: {
-        //        click: 'onLogoClick'
-        //    }
-        //}
+            {
+                iconCls: 'md-fullscreen',
+                handler: 'onToggleFullScreen'
+            },{
+                iconCls: 'x-fa fa-gear',
+                bind:{
+                    hidden: '{isAdmin}'
+                },
+                handler: 'onSettingsAdminSelect'
+            }
+            //,{
+            //    style:'background-image: url(/static/images/TokBoxIcon.png) !important; background-size: 29px 29px; background-repeat: no-repeat; ',
+            //    plain: true,
+            //    listeners: {
+            //        click: 'onLogoClick'
+            //    }
+            //}
 
         ]
     },
