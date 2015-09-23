c420d2a88fcfd369b991c84b77c48478eeaca99a
bradschafer <brad@theschafergroup.com>
2015-09-23 11:24:55 -0700

    room members work

--- WebRTC/app/model/chat/RoomMember.js
+++ WebRTC/app/model/chat/RoomMember.js
@@ -1,8 +1,8 @@
 Ext.define('WebRTC.model.chat.RoomMember', {
     extend: 'Ext.data.Model',
-    idProperty: 'id',
-    requires: ['Ext.data.identifier.Uuid'],
-    identifier: 'uuid', //creates a uuid and assisgns it to the id field
+    //idProperty: 'id',
+    //requires: ['Ext.data.identifier.Uuid'],
+    //identifier: 'uuid', //creates a uuid and assisgns it to the id field
     fields: [
         'id',
         'roomid',
@@ -11,8 +11,7 @@ Ext.define('WebRTC.model.chat.RoomMember', {
         { name: 'isBroadcasting', type: 'boolean', defaultValue: false}
     ],
     proxy: {
-        type: 'memory'
-        /*
+        // type: 'memory'
         type: 'socketio',
         url : '/roommembers',
         extraParams: {
@@ -31,6 +30,5 @@ Ext.define('WebRTC.model.chat.RoomMember', {
             type: 'json',
             writeAllFields: true
         }
-        */
     }
 });
